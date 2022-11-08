b9069e3ae596575b8f908df7306d44c851b31a55
unknown <ehdro0650@naver.com>
2022-11-08 12:31:33 +0900

    image

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import numpy as np
 # 헤드라인
 st.write("# 보험료 예측")
 st.write("**내 보험료는 얼마일까?**")
-st.iamge("https://data.ac-illust.com/data/thumbnails/7e/7e28e35c383edf317e8a92084e591aa5_t.jpeg")
+st.image("https://data.ac-illust.com/data/thumbnails/7e/7e28e35c383edf317e8a92084e591aa5_t.jpeg")
 
 # 첫번째 행
 r1_col1, r1_col2, r1_col3 = st.columns(3)
