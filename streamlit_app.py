6832ade0374e87e0f8129799dfa7e6ddcbaf3cdc
unknown <ehdro0650@naver.com>
2022-11-08 14:02:51 +0900

    ?

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.image("https://data.ac-illust.com/data/thumbnails/7e/7e28e35c383edf317e8a9208
 # 첫번째 행
 r1_col1 , r1_col2 , r1_col3 = st.columns(3)
 
-age = r1_col1.number_input("age", step=1, value=23).write(":baby:")
+age = r1_col1.number_input("age", step=1, value=23)
 
 bmi = r1_col2.number_input("bmi", value=34.40)
 
