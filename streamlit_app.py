db91b5aace09f9d9d8f50e2d97320b05ecab01e9
unknown <ehdro0650@naver.com>
2022-11-08 13:37:24 +0900

    asf

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,9 @@ import numpy as np
 st.write("# 보험료 예측")
 st.write("## ** *내 보험료는 얼마일까?* **")
 st.image("https://data.ac-illust.com/data/thumbnails/7e/7e28e35c383edf317e8a92084e591aa5_t.jpeg")
-# :pager:
+
 # 첫번째 행
-r1_col1 , r1_col2 , r1_col3 = st.columns(3)
+r1_col1 :baby: , r1_col2 , r1_col3 = st.columns(3)
 
 age = r1_col1.number_input("age", step=1, value=23)
 
